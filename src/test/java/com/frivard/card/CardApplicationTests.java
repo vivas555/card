8e5d21b4b727250961abb034969dccad1a1618ca
V <rivardfelix@gmail.com>
2022-09-22 12:49:17 -0400

    Initial commit

---
+++ src/test/java/com/frivard/card/CardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frivard.card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
